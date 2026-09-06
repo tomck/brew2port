b9f731a2b7d0cfa9b5a07c9eda6c36d8a8f25506
Tom Koch <tomck@users.noreply.github.com>
2026-09-06 00:27:49 -0500

    Use Intel-bottled Python 3.14

--- Formula/brew2port.rb
+++ Formula/brew2port.rb
@@ -5,7 +5,7 @@ class Brew2port < Formula
   url "https://github.com/tomck/homebrew-brew2port/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "686e700d86fc4bf78fc98976773365498902ef1cd6ee83da7ae47d110a7c67ce"
   license "MIT"
-  depends_on "python@3.12"
+  depends_on "python@3.14"
 
   def install
     virtualenv_install_with_resources
